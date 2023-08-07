77bff7c9ed51361e08cf166ba426d190274c7ab1
amin1029384756 <52065383+amin1029384756@users.noreply.github.com>
2023-08-07 16:55:53 -0700

    Update vcpu2.py

---
+++ vcpu/vcpu2.py
@@ -0,0 +1,84 @@
+class VirtualCPU:
+    def __init__(self):
+        # Define two registers and a result register
+        self.registerA = 0
+        self.registerB = 0
+        self.result = 0
+        self.instruction_pointer = 0
+        self.instructions = []
+
+    def load(self, register, value):
+        if register == 'A':
+            self.registerA = value
+        elif register == 'B':
+            self.registerB = value
+
+    def add(self):
+        self.result = self.registerA + self.registerB
+
+    def subtract(self):
+        self.result = self.registerA - self.registerB
+
+    def multiply(self):
+        self.result = self.registerA * self.registerB
+
+    def divide(self):
+        if self.registerB != 0:
+            self.result = self.registerA / self.registerB
+        else:
+            print("Error: Division by zero!")
+            self.halt()
+
+    def jump(self, condition, address):
+        if condition == 'zero' and self.result == 0:
+            self.instruction_pointer = address
+        elif condition == 'positive' and self.result > 0:
+            self.instruction_pointer = address
+        elif condition == 'negative' and self.result < 0:
+            self.instruction_pointer = address
+
+    def halt(self):
+        self.instruction_pointer = len(self.instructions)
+
+    def execute(self, instructions):
+        self.instructions = instructions
+        while self.instruction_pointer < len(self.instructions):
+            instruction = self.instructions[self.instruction_pointer]
+            if instruction[0] == 'LOAD':
+                self.load(instruction[1], instruction[2])
+            elif instruction[0] == 'ADD':
+                self.add()
+            elif instruction[0] == 'SUB':
+                self.subtract()
+            elif instruction[0] == 'MUL':
+                self.multiply()
+            elif instruction[0] == 'DIV':
+                self.divide()
+            elif instruction[0] == 'JUMP':
+                self.jump(instruction[1], instruction[2])
+            elif instruction[0] == 'HALT':
+                self.halt()
+            self.instruction_pointer += 1
+
+    def print_register(self, register):
+        if register == 'A':
+            print(self.registerA)
+        elif register == 'B':
+            print(self.registerB)
+        elif register == 'result':
+            print(self.result)
+
+# Test the virtual CPU
+instructions = [
+    ('LOAD', 'A', 5),
+    ('LOAD', 'B', 3),
+    ('SUB',),
+    ('JUMP', 'negative', 7),
+    ('LOAD', 'A', 10),
+    ('LOAD', 'B', 2),
+    ('DIV',),
+    ('HALT',)
+]
+cpu = VirtualCPU()
+cpu.execute(instructions)
+cpu.print_register('result')  # Expected output: 5 (because 10 divided by 2 is 5)
